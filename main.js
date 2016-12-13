751be6f0f3c473c4ca537d8890054d7c11e9266a
Yannick Van Oekelen <r0298651@student.thomasmore.be>
2016-12-13 10:59:37 +0100

    Plaatsen van de code van vorige week in het project Start van aanpassing aan huidige code

---
+++ main.js
@@ -0,0 +1,89 @@
+/**
+ * Created by yannickvanoekelen on 13/12/16.
+ */
+<!-- In samenwerking met genzo, jelle & kevin !-->
+<!-- Deze verzie is een 2.0 versie, vorige project was foutief opgeslagen met corruptie tot gevolg !-->
+
+<!DOCTYPE html>
+<html>
+<body> <!-- Opmaak bepalen !-->
+<style media="screen">
+    h1, p {
+    font-family: 'Roboto', sans-serif;
+}
+table {
+    border-collapse: collapse;
+    width: 100%;
+    font-family: 'Roboto', sans-serif;
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+tr:hover {
+    background-color: #f5f5f5;
+}
+</style>
+<!-- Hoofding en tabel verwezelijken !-->
+<div>
+<h1>EX-7 Drone data: Yannick Van Oekelen</h1>
+<p>Loading the data, please have some patience it takes a while </p>
+<table id="dronedata">
+    <tr>
+    <th>DRONE NAME</th>
+<th>DRONE MAC ADDRESS</th>
+<th>FILE  ID</th>
+<th>FIRST RECORD</th>
+<th>LAST RECORD</th>
+</tr>
+</table>
+</div>
+</body>
+
+<head> <!-- Gegevens in tabel plaatsen !-->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>EX-7 Drone data: Yannick Van Oekelen</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript">
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://web-ims.thomasmore.be/datadistribution/API/2.0/drones?format=json",
+    "method": "GET",
+    "dataType": 'json',
+    "headers": {
+        "authorization": "Basic aW1zOno1MTJtVDRKeVgwUExXZw==",
+        "cache-control": "no-cache"
+    }
+};
+$.ajax(settings)
+    .done(function(response_1) {
+        for (var x = 0; x < response_1.length; x++) {
+            settings['url'] = response_1[x]['url'];
+            $.ajax(settings).done(function(response_2) {
+                settings['url'] = 'https://web-ims.thomasmore.be/datadistribution/API/2.0/files?drone_id.is=' + response_2['id'] + '&format=json';
+                $.ajax(settings).done(function(response_3) {
+                    for (var y = 0; y < response_3.length; y++) {
+                        settings['url'] = response_3[y]['url'];
+                        $.ajax(settings).done(function(response_4) {
+                            $("#dronedata").append(
+                                '<tr>' +
+                                '<td>' + response_2.name + '</td>' +
+                                '<td>' + response_2.mac_address + '</td>' +
+                                '<td>' + response_4.id + '</td>' +
+                                '<td>' + response_4.date_first_record + '</td>' +
+                                '<td>' + response_4.date_last_record + '</td>' +
+                                '</tr>');
+                        });
+                    };
+                });
+            });
+        };
+    });
+</script>
+</head>
+
+</html>
