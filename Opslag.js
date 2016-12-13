2ffb59736fc8e75a671dfad10e97794214ed6de3
Yannick Van Oekelen <r0298651@student.thomasmore.be>
2016-12-13 11:23:38 +0100

    Uitleg hoe te werk gegaan

--- Opslag.js
+++ Opslag.js
@@ -1,7 +1,14 @@
 /**
  * Created by yannickvanoekelen on 13/12/16.
  */
+// Eerst op papier uitgewerkt
+// vervolgens overleg gehad met Genzo, Jelle & Jeroen
+// Vervolgens stapje per stapje uitgewerkt (met hulp van bovenstaande partijen)
+// Vervolgens project eerst volledig uitgewerkt zelfstandig (geen committer)
+// Tenslotte project nu volledig hermaken met stukje per stukje te committen
 
 
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/prober';
+
+
